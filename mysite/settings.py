4a629893301c6bc8908bdd9d86af2665b9a9451b
fabalmeida <fabiola_almeida03@yahoo.com.br>
2018-03-10 16:41:31 -0300

    NOVA PERMISSÃO

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'r1)t)41=myh)^u+yhnqsj5(kxo0di21*88b1@w+f7zx-5r#!l&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]','fabiolaalmeida03.pythonanywhere.com']
 
 
 # Application definition
